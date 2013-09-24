1c4203d2c34397b3eebe6938fdccd0dcd3abfbca
kor <kor.dwarshuis@gmail.com>
2013-09-24 22:42:51 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+    <meta name="apple-mobile-web-app-title" content="background360">
+    <title>background360</title>
+    <link href="css/general.css" rel="stylesheet">
+    <script src="js/verge.min.js"></script>
+</head>
+<body>
+<div id="rotatemessage"><h1>Please hold device in landscape mode.</h1></div>
+<script src="js/background360.js"></script>
+</body>
+</html>
---
+++ js/background360.js
@@ -0,0 +1,43 @@
+var background360 = (function () {
+    var backgroundImageDimensions = {
+        width: 0,
+        height: 0
+    };
+//    var viewportWidth = verge.viewportW();
+    var viewportHeight = verge.viewportH();
+    var rotateX = function (deg1) {
+        document.querySelector("body").style.backgroundPositionX = deg1 * backgroundImageDimensions.width / 360 - 643 + "px";
+    };
+    var rotateY = function (deg2) {
+        document.querySelector("body").style.backgroundPositionY = -backgroundImageDimensions.height + viewportHeight -
+                deg2 * (backgroundImageDimensions.height / 340) + "px";
+    };
+
+    document.querySelector("body").style.backgroundRepeat = "repeat-x";
+
+    function init() {
+        if (window.DeviceOrientationEvent) {
+            window.addEventListener("deviceorientation", function (e) {
+                rotateX(e.alpha);
+                rotateY(e.gamma);
+            }, false);
+        }
+    }
+
+    return {
+        setBackgroundImage: function (backgroundImage) {
+            document.querySelector("body").style.backgroundImage = backgroundImage;
+        },
+        setBackgroundImageDimensions: function (theDimensions) {
+            backgroundImageDimensions.width = theDimensions.width;
+            backgroundImageDimensions.height = theDimensions.height;
+        },
+        init: init
+    };
+}());
+
+// configuration
+background360.setBackgroundImageDimensions({width: 2000, height: 966}); // the width and height of the background image
+background360.setBackgroundImage('url("img/360-background.jpg")'); // the background image
+// start
+background360.init();
